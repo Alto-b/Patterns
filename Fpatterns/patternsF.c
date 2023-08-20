a5a2d2674cac08c70e7ebdb3b7a087a560ddbac3
Alto b <altob282@gmail.com>
2023-08-20 08:25:27 +0530

    updated on 19

---
+++ Fpatterns/patternsF.c
@@ -0,0 +1,214 @@
+
+//codes for each pattern are shown below them 
+//uncomment individual codes for execution 
+//patterns are also commented using /* */
+
+
+
+
+/*
+
+**
+*
+*
+*
+****
+*
+*
+*
+*
+*
+*
+******
+
+
+*/
+
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k;
+//     for(i=0;i<=3;i++){
+//         for(j=1;j<=i*2;j++){
+//             printf("*");
+            
+            
+//         }printf("\n");
+//         for(k=1;k<=i*3;k++){
+//             if(i==3)
+//             break;
+
+//             printf("*");
+//             printf("\n");
+//         }
+//     }printf("\n");
+// }
+
+
+/*
+
+*
+****
+*
+*
+********
+*
+*
+*
+************
+
+*/
+
+//  #include<stdio.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=3;i++){
+//         for(j=1;j<=i;j++){
+//             printf("*"); 
+//             printf("\n");           
+//         }
+       
+//         for(k=1;k<=i*4;k++){
+            
+//             printf("*");
+//         }printf("\n");
+//     }printf("\n");
+// }
+
+
+/*
+
+
+***
+*
+******
+*
+*
+*********
+*
+*
+*
+
+
+*/
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=3;i++){
+//         for(j=1;j<=i*3;j++){
+//             printf("*"); 
+                       
+//         }printf("\n");
+       
+//         for(k=1;k<=i;k++){
+            
+//             printf("*");
+//             printf("\n");
+//         }
+//     }
+// }
+
+
+/*
+
+***
+*
+*
+*
+******
+*
+*
+*
+*
+*
+*
+*********
+
+*/
+
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=3;i++){
+//         for(j=1;j<=i*3;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+    
+//     for(k=1;k<=i*3;k++){
+//         if(i==3)
+//         break; 
+
+//         printf("*");
+//          printf("\n");
+//     }
+//    }
+//     }
+
+
+
+/*
+
+*****
+*
+*
+*
+**********
+*
+*
+*
+*
+*
+*
+***************
+
+*/
+
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=3;i++){
+//         for(j=1;j<=i*5;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+    
+//     for(k=1;k<=i*3;k++){
+//         if(i==3)
+//         break;
+         
+
+//         printf("*");
+//          printf("\n");
+//     }
+    
+//    }
+//     }
+
+
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int i,j,k;
+    for(i=1;i<=3;i++){
+        for(j=1;j<=i*5;j++){
+            printf("*");
+        }
+        printf("\n");
+    
+    for(k=1;k<=i*3;k++){
+        if(i==3)
+        break;
+         
+
+        printf("*");
+         printf("\n");
+    }
+    
+   }
+    }
+ 
---
+++ Fpatterns/trial.c
@@ -0,0 +1,3 @@
+int i=2,j;
+j=i^2;
+printf("%d",j);
