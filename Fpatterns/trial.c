627c10bcd5ae859e834259e02f87968807c6aca8
Alto b <altob282@gmail.com>
2023-08-20 11:25:10 +0530

    update on 20

--- Fpatterns/trial.c
+++ Fpatterns/trial.c
@@ -1,3 +1,30 @@
-int i=2,j;
-j=i^2;
-printf("%d",j);
+#include<stdio.h>
+
+int main() {
+    int i, j, k;
+    
+    for (i = 1; i <= 3; i++) {
+        for (j = 1; j <= i * 6; j++) {
+            if (i == 3 && j <= 6) {
+                printf("*");
+            } else {
+                printf("j");
+            }
+            if (j % 3 == 0) {
+                printf("\n");
+            }
+        }
+        
+        printf("\n");
+        
+        for (k = 1; k <= i; k++) {
+            if (i == 3) {
+                break;
+            }
+            printf("k");
+            printf("\n");
+        }
+    }
+    
+    return 0;
+}
